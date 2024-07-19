bb0b9f9e1fed700699cc76062dcd9f55bd61cfe7
Pranay Sherkar <116248469+Pranaysherkar@users.noreply.github.com>
2024-07-20 00:05:30 +0530

    stage one completed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,46 +2,53 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='h-screen pt-[10vh]'>
-      <div className='flex gap-[1%] h-full'>
-        <div className='h-full w-[59.5%] flex'>
-          <div className='w-1/2 h-full'>
-            <div className='h-1/2 border-b-[1px] border-zinc-800 w-full flex flex-col items-center justify-center'>
-              <h1 className='text-6xl font-black'>Xiaomi</h1>
+    <div className='min-h-sc lg:h-screen w-full pt-[10vh]'>
+
+      <div className='flex flex-col lg:flex-row gap-[1%] h-full'>
+
+        <div className='h-full w-full lg:w-[59.5%] flex flex-col lg:flex-row'>
+          <div className='w-full lg:w-1/2 h-full'>
+            <div className='h-1/2 border-b-[1px] border-zinc-800 w-full flex flex-col items-center justify-start'>
+              <h1 className='text-3xl sm:text-4xl lg:text-6xl font-black'>Xiaomi</h1>
               <p className='text-sm'>India's No.1 Smartphone Brand</p>
             </div>
-            <div className='h-1/2 border-b-[1px] border-zinc-800 w-full px-20 flex justify-center flex-col gap-6'>
-              <p className='leading-none'>Get Attractive Offers For Your First Purchase.</p>
-              <button className='px-6 py-2 bg-zinc-200 text-black'>Explore Now</button>
+            <div className='h-1/2 border-b-[1px] border-zinc-800 w-full px-5 sm:px-10 lg:px-20 flex justify-center flex-col gap-6'>
+              <p className='leading-none text-center sm:text-left'>Get Attractive Offers For Your First Purchase.</p>
+              <button className='px-6 py-2 mb-5 rounded-md bg-zinc-200 text-black self-center sm:self-start'>Explore Now</button>
             </div>
           </div>
-          <div className='relative w-1/2 h-full border-x-[1px] border-zinc-800'>
+          
+          <div className='relative w-full lg:w-1/2 h-full border-x-[1px] border-zinc-800'>
             <img className='h-full w-full object-cover' src="https://raw.githubusercontent.com/xvsanuj/XiaomiReimagined/main/src/assets/Images/Home/4.jpg" alt="" />
-            <div className='absolute bg-[#55504c] bottom-5  left-5 h-28 w-72'>
-              <h1 className='bg-[#55504c] font-normal uppercase text-2xl tracking-tighter pt-3 px-5'>every shot iconic</h1>
-              <span className='h-[1px] w-[80%] bg-zinc-200 inline-block mx-5'></span>
-              <div className='flex items-center bg-[#55504c] px-5 justify-between'>
+            
+            <div className='absolute bg-[#55504c] bottom-5 left-5 h-28 w-68 lg:w-72 sm:block rounded-md overflow-hidden'>
+              <h1 className='bg-[#55504c] font-normal uppercase text-xl lg:text-2xl tracking-wide lg:tracking-tighter pt-3 px-5'>every shot iconic</h1>
+              <span className='h-[1px] w-[90%] lg:w-[80%] bg-zinc-200 inline-block lg:mx-5 mx-3'></span>
+              <div className='flex items-center bg-[#55504c] lg:px-5 px-2 justify-between lg:my-0 my-2'>
                 <h1 className='bg-[#55504c] leading-8'>Quick View</h1>
                 <button className='bg-[#55504c] px-6 py-1 border-[1px] rounded-full mr-4'>Buy Now</button>
               </div>
             </div>
           </div>
         </div>
-        <div className='h-full flex w-[39.5%] border-x-[1px] border-zinc-800'>
-          <div className='w-[80%] h-full flex flex-col gap-[3%] border-r-[1px] border-zinc-800'>
+
+        <div className='h-full flex w-full lg:w-[39.5%] border-x-[1px] border-zinc-800 flex-col lg:flex-row'>
+          <div className='w-full lg:w-[80%] h-full flex flex-col gap-[3%] border-r-[1px] border-zinc-800'>
             <div className='h-[59%] w-full border-b-[1px] border-zinc-800'>
               <img className='h-full w-full object-cover' src="https://raw.githubusercontent.com/xvsanuj/XiaomiReimagined/main/src/assets/Images/Home/3.jpg" alt="" />
             </div>
             <div className='h-[38%] w-full border-y-[1px] border-zinc-800'>
-            <img className='h-full w-full object-cover' src="https://raw.githubusercontent.com/xvsanuj/XiaomiReimagined/main/src/assets/Images/Home/2.jpg" alt="" />
+              <img className='h-full w-full object-cover' src="https://raw.githubusercontent.com/xvsanuj/XiaomiReimagined/main/src/assets/Images/Home/2.jpg" alt="" />
             </div>
           </div>
-          <div className='h-full flex items-center justify-between py-20 flex-col w-[20%]'>
-            <h1 className='-rotate-90 origin-left whitespace-nowrap mt-36 ml-48 font-semibold'>Scroll Down To Next Page</h1>
-            <h1 className='-rotate-90 text-2xl font-black whitespace-nowrap origin-left tracking-tighter ml-16'>PAGE 1</h1>
+          <div className='hidden h-full lg:flex items-center justify-between py-10 lg:py-20 flex-col w-full lg:w-[20%]'>
+            <h1 className='-rotate-90 origin-left whitespace-nowrap mt-12 lg:mt-36 lg:ml-48 font-semibold text-xs sm:text-sm lg:text-base'>Scroll Down To Next Page</h1>
+            <h1 className='-rotate-90 text-base sm:text-xl lg:text-2xl font-black whitespace-nowrap origin-left tracking-tighter lg:ml-16'>PAGE 1</h1>
           </div>
         </div>
+        
       </div>
+
     </div>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,29 +1,42 @@
 import { CiSearch } from "react-icons/ci";
 const Navbar = () => {
-  const navLinks = ["Store", "Phones", "Tablets", "TV & Smart Home", "Smart Watch & Audio"];
+  const navLinks = [
+    "Store",
+    "Phones",
+    "Tablets",
+    "TV & Smart Home",
+    "Smart Watch & Audio",
+  ];
   return (
-    <div className="h-[10vh] fixed justify-between flex items-center px-20 w-full border-b-[1px] border-zinc-800 z-50">
-      <div className="w-[25%] flex items-center gap-4">
-        <h1 className="text-3xl font-semibold tracking-tighter">Xiaomi</h1>
-        <span className="h-8 w-[3px] inline-block bg-zinc-800 rounded-full"></span>
-        <p className="text-xs font-semibold leading-none">India's No1 <br />Smartphone Brand</p>
+    <div className="h-12 lg:h-[10vh] fixed justify-between flex items-center px-5 lg:px-20 w-full border-b-[1px] border-zinc-800 z-50">
+      <div className="lg:w-[25%] flex items-center gap-4">
+        <h1 className="text-lg tracking-wide lg:text-3xl font-semibold lg:tracking-tight">
+          Xiaomi
+        </h1>
+        <span className="h-5 lg:h-8 w-[3px] inline-block bg-zinc-800 rounded-full"></span>
+        <p className="text-[8px] lg:text-xs font-thin lg:font-semibold leading-none">
+          India's No1 <br />
+          Smartphone Brand
+        </p>
       </div>
-      <div className="w-[50%] flex justify-center gap-6">
-        {
-          navLinks.map((link, index) => (
-            <h1 className="cursor-pointer text-sm">{link}</h1>
-          ))
-        }
+      <div className="hidden w-[50%] lg:flex justify-center gap-6">
+        {navLinks.map((link, index) => (
+          <h1 key={index} className="cursor-pointer text-sm">
+            {link}
+          </h1>
+        ))}
       </div>
-      <div className="w-[25%] flex items-center gap-8 justify-end">
-        <span className="text-2xl cursor-pointer"><CiSearch/></span>
-        <div className="h-3 cursor-pointer flex flex-col justify-between">
+      <div className="w-[18%] lg:w-[25%] flex lg:items-center lg:gap-8 justify-end">
+        <span className="hidden lg:block text-2xl cursor-pointer">
+          <CiSearch />
+        </span>
+        <div className="h-2 lg:h-3 cursor-pointer flex flex-col justify-between">
           <span className="inline-block h-[.1rem] bg-white w-10"></span>
           <span className="inline-block h-[.1rem] bg-white w-10"></span>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
