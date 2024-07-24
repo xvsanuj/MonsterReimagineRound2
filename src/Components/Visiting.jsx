40ec1985b9ed1f8adae3236c94b6ba4d15d7332e
xvsanuj <anuj.shar2k8@gmail.com>
2024-07-24 10:17:56 +0530

    Final Structure Commit

--- src/Components/Visiting.jsx
+++ src/Components/Visiting.jsx
@@ -21,8 +21,9 @@ const Visiting = () => {
     return ( 
         <div className='hello relative h-[200vh] w-full overflow-x-hidden'> 
             <div className="elems-visit w-full h-screen flex"> 
-                <div className='elem h-full shrink-0 w-[40%] border-l-[1px] border-zinc-800'> 
-                    <h1 className='text-7xl tracking-tighter font-[Monument] w-72 pt-48 pl-28'>Thanks For Visiting</h1> 
+                <div className='elem relative h-full shrink-0 w-[40%] border-l-[1px] border-zinc-800'> 
+                    <h1 className='text-6xl text-center tracking-tighter font-[Monument] w-72 pt-48 pl-28'>Thanks For Visiting.</h1> 
+                    <img className="absolute top-[55%] left-[45%] -translate-x-2/4 h-40 w-40 object-cover" src="https://cdn3d.iconscout.com/3d/premium/thumb/heart-8190390-6507858.png" alt="" />
                 </div> 
                 <div className='elem h-full shrink-0 w-[20%] border-l-[1px] border-zinc-800'> 
                     <h1 className='text-7xl tracking-tighter font-[Monument] rotate-90 mt-64'>Smart <br /> Bundles</h1> 
