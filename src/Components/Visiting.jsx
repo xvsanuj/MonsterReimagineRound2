b3d746675fc66c745c0f3c6d05b85db831f78c10
xvsanuj <anuj.shar2k8@gmail.com>
2024-07-25 06:57:42 +0530

    Home Page Final

--- src/Components/Visiting.jsx
+++ src/Components/Visiting.jsx
@@ -22,29 +22,29 @@ const Visiting = () => {
         <div className='hello relative h-[200vh] w-full overflow-x-hidden'> 
             <div className="elems-visit w-full h-screen flex"> 
                 <div className='elem relative h-full shrink-0 w-[40%] border-l-[1px] border-zinc-800'> 
-                    <h1 className='text-6xl text-center tracking-tighter font-[Monument] w-72 pt-48 pl-28'>Thanks For Visiting.</h1> 
+                    <h1 className='text-6xl text-center tracking-tighter font-[Monument] w-72 pt-32 pl-28'>Thanks For Visiting.</h1> 
                     <img className="absolute top-[55%] left-[45%] -translate-x-2/4 h-40 w-40 object-cover" src="https://cdn3d.iconscout.com/3d/premium/thumb/heart-8190390-6507858.png" alt="" />
                 </div> 
                 <div className='elem h-full shrink-0 w-[20%] border-l-[1px] border-zinc-800'> 
-                    <h1 className='text-7xl tracking-tighter font-[Monument] rotate-90 mt-64'>Smart <br /> Bundles</h1> 
+                    <h1 className='text-7xl tracking-tighter font-[Monument] rotate-90 mt-52'>Smart <br /> Bundles</h1> 
                 </div> 
                 <div className='elem h-full shrink-0 w-[20%] border-l-[1px] border-zinc-800'> 
-                    <h1 className='text-7xl tracking-tighter font-[Monument] rotate-90 mt-64'>Exclusive <br /> Coupons</h1> 
+                    <h1 className='text-7xl tracking-tighter font-[Monument] rotate-90 mt-52'>Exclusive <br /> Coupons</h1> 
                 </div> 
                 <div className='elem h-full shrink-0 w-[20%] border-l-[1px] border-zinc-800'> 
-                    <h1 className='text-7xl tracking-tighter font-[Monument] rotate-90 mt-64'>Smart <br /> TV</h1> 
+                    <h1 className='text-7xl tracking-tighter font-[Monument] rotate-90 mt-52'>Smart <br /> TV</h1> 
                 </div> 
                 <div className='elem h-full shrink-0 w-[20%] border-l-[1px] border-zinc-800'> 
-                    <h1 className='text-7xl tracking-tighter font-[Monument] rotate-90 mt-64'>Power <br /> Bank</h1> 
+                    <h1 className='text-7xl tracking-tighter font-[Monument] rotate-90 mt-52'>Power <br /> Bank</h1> 
                 </div> 
                 <div className='elem h-full shrink-0 w-[20%] border-l-[1px] border-zinc-800'> 
-                    <h1 className='text-7xl tracking-tighter font-[Monument] rotate-90 mt-64'>Audio <br /> Products</h1> 
+                    <h1 className='text-7xl tracking-tighter font-[Monument] rotate-90 mt-52'>Audio <br /> Products</h1> 
                 </div> 
                 <div className='elem h-full shrink-0 w-[20%] border-l-[1px] border-zinc-800'> 
-                    <h1 className='text-6xl tracking-tighter font-[Monument] rotate-90 mt-64'>Bestselling <br /> Smartphone's</h1> 
+                    <h1 className='text-6xl tracking-tighter font-[Monument] rotate-90 mt-52'>Bestselling <br /> Smartphone's</h1> 
                 </div> 
                 <div className='elem h-full shrink-0 w-[20%] border-l-[1px] border-zinc-800'> 
-                    <h1 className='text-6xl tracking-tighter font-[Monument] rotate-90 mt-64'>No1 <br /> Brand</h1> 
+                    <h1 className='text-6xl tracking-tighter font-[Monument] rotate-90 mt-52'>No1 <br /> Brand</h1> 
                 </div> 
             </div> 
         </div> 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    extend: {
+      animation: {
+        'zoom-in': 'zoomIn 0.5s ease-in-out',
+      },
+      keyframes: {
+        zoomIn: {
+          '0%': { transform: 'scale(1)' },
+          '100%': { transform: 'scale(1.1)' },
+        },
+      },
+    },
+  },
+  varients: {
     extend: {},
   },
   plugins: [],
 }
-
